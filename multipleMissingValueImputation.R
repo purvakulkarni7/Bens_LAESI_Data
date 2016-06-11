1726b20116df2604dc6f68d7b371f54d48f65891
purva <pkulkarni@ice.mpg.de>
2016-06-11 23:45:14 +0200

    Another try using K (WIP)

--- multipleMissingValueImputation.R
+++ multipleMissingValueImputation.R
@@ -14,7 +14,7 @@ library(tools) # unless already loaded, comes with base R
 
 #' Title
 #'
-#' @param filePath 
+#' @param filePath
 #'
 #' @return
 #' @export
@@ -23,8 +23,10 @@ library(tools) # unless already loaded, comes with base R
 multipleMissingValueImputation <- function(filePath)
 {
   ## Read .csv file
-  fileData <-read.csv(filePath, header = FALSE) # Reading csv file with no headers
-  fileName <- file_path_sans_ext(basename(filePath)) # Gets the name of the file without extension
+  fileData <-
+    read.csv(filePath, header = FALSE) # Reading csv file with no headers
+  fileName <-
+    file_path_sans_ext(basename(filePath)) # Gets the name of the file without extension
   dirPath <- dirname(filePath) # Get the path to the directory
   
   ## Convert the .csv file to matrix class
@@ -38,47 +40,54 @@ multipleMissingValueImputation <- function(filePath)
   newDataMatrix = matrix(nrow = numberOfRows, ncol = numberOfColumns)
   
   ## missing value counter
-  missingValueCount = 1;
+  missingValueCount = 0;
   incrementValue = 1;
   
   ## Access each value of the dataMatrix, check if it -10
-  for (i in 1:numberOfRows) # for each row
+  for (i in 1:numberOfRows)  # for each row
   {
     for (j in 1:numberOfColumns) # for each column
     {
-      if (dataMatrix[i,j] == -10) # For single cell missing value
+      k = j;
+      if (dataMatrix[i,j] == -10)  # For single cell missing value
       {
-       # cat("Missing values: ", missingValueCount, "at (i,j) ---> ", i, ",", j, "\n")
-        while (j <= numberOfColumns)
+        cat("value of j ABOVE HERE is: ", j, "\n")
+        cat(
+          "I got a single missing value at (",i,",",j,". Is there a consecutive missing value?", "\n"
+        )
+        for (k in j:numberOfColumns)
         {
-          if (dataMatrix[i,j + incrementValue] == -10)
+          if (dataMatrix[i,k + incrementValue] == -10)
           {
+            cat("Yes there is.", "\n")
             missingValueCount = missingValueCount + 1; # missing value count increases by 1
             incrementValue = incrementValue + 1;
-            cat("Missing values: ", missingValueCount, "at (i,j) ---> ", i, ",", j, "\n")
-            next;
           }
           else
           {
-           # cat("Total consecutive missing values: ", missingValueCount, "at (i,j) ---> ", i, ",", j, "\n")
+            cat("No there is no consecutive value.","\n")
             break;
           }
         }
+        cat("Mising values here: ", missingValueCount, "\n")
+        next;
       }
       else
       {
         newDataMatrix[i,j] = dataMatrix[i,j]; # value remains unchanged for non-missing values
       }
-      #  j = j + incrementValue;
-      missingValueCount = 1;
+      missingValueCount = 0;
       incrementValue = 1;
+      next;
     }
   }
   
   ## Generate file name and path where the new matrix wll be written
-  outputFilePath = paste(dirPath, "/", fileName,"Imputed.csv", sep = "") 
+  outputFilePath = paste(dirPath, "/", fileName,"Imputed.csv", sep = "")
   # Note: Use forward or backward slash based on OS
   
   ## Write the matrix to the file
-  write.table(newDataMatrix, file = outputFilePath, sep = ",", row.names = FALSE, col.names = FALSE)
+  write.table(
+    newDataMatrix, file = outputFilePath, sep = ",", row.names = FALSE, col.names = FALSE
+  )
 }
